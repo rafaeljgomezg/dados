01a1176ade9704d68b4968f46af0ff1ead2d554f
rafaeljgomezg <rafaeljgomezg@gmail.com>
2023-08-13 04:54:43 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dados';
+  
+  numero1:number =  Math.round(Math.random() * 5 + 1)
+  numero2:number =  Math.round(Math.random() * 5 + 1)
+
+  dadoIzquierda="../assets/img/dice"+this.numero1+".png"
+  dadoDerecha="../assets/img/dice"+this.numero2+".png"
+  ganaste:boolean=false
+
+
+  tirarDados(){
+    this.numero1 = Math.round(Math.random() * 5 + 1)  //genero numero entre 1 y 6 aleatorios
+    this.numero2 = Math.round(Math.random() * 5 + 1)  //genero numero entre 1 y 6 aleatorios
+
+    this.dadoDerecha="../assets/img/dice"+this.numero1+".png"
+    this.dadoIzquierda="../assets/img/dice"+this.numero2+".png"
+  }
+
+
 }
